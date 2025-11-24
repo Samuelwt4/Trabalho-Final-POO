f8c0a86988db406fb8c2417d981bfdd50f2a0f6d
Samuelwt4 <samuka.watthier@gmail.com>
2025-11-23 23:11:22 -0300

    parte final usuários

--- usuarios/Usuario.java
+++ usuarios/Usuario.java
@@ -1,3 +1,5 @@
+package usuarios;
+
 public abstract class Usuario {
 
     private int id;
--- usuarios/UsuarioIndividual.java
+++ usuarios/UsuarioIndividual.java
@@ -1,3 +1,5 @@
+package usuarios;
+
 public class UsuarioIndividual extends Usuario {
     private String cpf;
 
